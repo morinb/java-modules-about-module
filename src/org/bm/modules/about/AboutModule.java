b46ce967ff051691f2a92b05eaf4d57cb201c8e9
Baptiste MORIN <morin.baptiste@gmail.com>
2012-11-21 21:51:07 +0100

    set eclipse policy to space only for indentation

--- src/org/bm/modules/about/AboutModule.java
+++ src/org/bm/modules/about/AboutModule.java
@@ -11,54 +11,57 @@ import org.bm.modules.shared.ModuleFrame;
 
 public class AboutModule implements IModule {
 
-   private final ModuleFrame frame = new AboutModuleFrame();
+    private final ModuleFrame frame = new AboutModuleFrame();
 
-   @Override
-   public void attach() {}
+    @Override
+    public void attach() {
+    }
 
-   @Override
-   public void deattach() {}
+    @Override
+    public void deattach() {
+    }
 
-   @Override
-   public int getMenuIndex() {
-      return IModule.MENU_HELP;
-   }
+    @Override
+    public int getMenuIndex() {
+        return IModule.MENU_HELP;
+    }
 
-   @Override
-   public int getMenuItemIndex() {
-      return 0;
-   }
+    @Override
+    public int getMenuItemIndex() {
+        return 0;
+    }
 
-   @Override
-   public String getName() {
-      return "About";
-   }
+    @Override
+    public String getName() {
+        return "About";
+    }
 
-   @Override
-   public boolean hasMnemonic() {
-      return true;
-   }
+    @Override
+    public boolean hasMnemonic() {
+        return true;
+    }
 
-   @Override
-   public int getMnemonic() {
-      // TODO Auto-generated method stub
-      return KeyEvent.VK_A;
-   }
+    @Override
+    public int getMnemonic() {
+        // TODO Auto-generated method stub
+        return KeyEvent.VK_A;
+    }
 
-   @Override
-   public boolean hasAccelerator() {
-      return true;
-   }
+    @Override
+    public boolean hasAccelerator() {
+        return true;
+    }
 
-   @Override
-   public KeyStroke getAccelerator() {
+    @Override
+    public KeyStroke getAccelerator() {
 
-      return KeyStroke.getKeyStroke(KeyEvent.VK_COMMA, InputEvent.CTRL_DOWN_MASK);
-   }
+        return KeyStroke.getKeyStroke(KeyEvent.VK_COMMA,
+                InputEvent.CTRL_DOWN_MASK);
+    }
 
-   @Override
-   public ModuleFrame getModuleFrame() {
-      return frame;
-   }
+    @Override
+    public ModuleFrame getModuleFrame() {
+        return frame;
+    }
 
 }
--- src/org/bm/modules/about/frame/AboutModuleFrame.java
+++ src/org/bm/modules/about/frame/AboutModuleFrame.java
@@ -3,10 +3,10 @@ package org.bm.modules.about.frame;
 import org.bm.modules.shared.ModuleFrame;
 
 public class AboutModuleFrame extends ModuleFrame {
-   public AboutModuleFrame() {
-      this.setClosable(true);
-      this.setIconifiable(true);
-      this.setMaximizable(true);
-      this.setSize(640, 480);
-   }
+    public AboutModuleFrame() {
+        this.setClosable(true);
+        this.setIconifiable(true);
+        this.setMaximizable(true);
+        this.setSize(640, 480);
+    }
 }
